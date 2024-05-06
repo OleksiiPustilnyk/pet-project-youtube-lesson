bd0d63f687c77aab32a42ea9ad2b74d7b0fafc9a
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-05-06 17:40:10 +0300

    Загрузка комментариев к посту

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,13 +7,20 @@ import Loader from '../components/UI/loader/Loader'
 const PostIdPage = () => {
     const params = useParams()
     const [post, setPost] = useState({})
-    const [fetchPostById, isLoading, error] = useFetching(async () => {
+    const [comments, setComments] = useState([])
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(params.id)
         setPost(response.data)
     })
 
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(params.id)
+        setComments(response.data)
+    })
+
     useEffect(() => {
         fetchPostById(params.id)
+        fetchComments(params.id)
     }, [])
     return (
         <div>
@@ -23,6 +30,20 @@ const PostIdPage = () => {
             ) : (
                 <div>
                     {post.id}, {post.title}
+                </div>
+            )}
+
+            <h1>Комментарии</h1>
+            {isComLoading ? (
+                <Loader />
+            ) : (
+                <div>
+                    {comments.map((comm) => (
+                        <div key={comm.id} style={{ marginTop: 15 }}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
