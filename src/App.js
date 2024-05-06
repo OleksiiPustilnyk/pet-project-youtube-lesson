1e633172d7607137d21d02622635a48bb8de69a6
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-05-06 12:37:29 +0300

    Индикация загрузки данных с сервера

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
     const [filter, setFilter] = useState({ sort: '', query: '' })
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [isPostsLoading, setIsPostsLoading] = useState(false)
 
     useEffect(() => {
         fetchPosts()
@@ -28,8 +29,12 @@ function App() {
     }
 
     async function fetchPosts() {
-        const posts = await PostService.getAll()
-        setPosts(posts)
+        setIsPostsLoading(true)
+        setTimeout(async () => {
+            const posts = await PostService.getAll()
+            setPosts(posts)
+            setIsPostsLoading(false)
+        }, 1000)
     }
 
     const removePost = (post) => {
@@ -50,11 +55,15 @@ function App() {
 
             <PostFilter filter={filter} setFilter={setFilter} />
 
-            <PostList
-                remove={removePost}
-                posts={sortedAndSearchedPosts}
-                title={'Список'}
-            />
+            {isPostsLoading ? (
+                <h1>Идет загрузка...</h1>
+            ) : (
+                <PostList
+                    remove={removePost}
+                    posts={sortedAndSearchedPosts}
+                    title={'Список'}
+                />
+            )}
         </div>
     )
 }
