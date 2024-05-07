c3595b4243232642e02b5cc4ca104a7dcc700583
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-05-07 11:45:24 +0300

    useContext. Глобальные данные. Авторизация пользователя

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { privateRoutes, publicRoutes } from '../router/index'
+import { AuthContext } from '../context'
+import Loader from './UI/loader/Loader'
 
 const AppRouter = () => {
-    const isAuth = true
+    const { isAuth, isLoading } = useContext(AuthContext)
+    console.log(isAuth)
+
+    if (isLoading) {
+        return <Loader />
+    }
 
     return isAuth ? (
-        <Routes>
-            {privateRoutes.map((route) => (
-                <Route
-                    element={<route.component />}
-                    path={route.path}
-                    key={route.path}
-                    exact={route.exact}
-                />
-            ))}
-        </Routes>
+        <>
+            <Routes>
+                {privateRoutes.map((route) => (
+                    <Route
+                        element={<route.component />}
+                        path={route.path}
+                        key={route.path}
+                        exact={route.exact}
+                    />
+                ))}
+            </Routes>
+
+            {/* <Navigate to="/posts" replace /> */}
+        </>
     ) : (
         <>
             <Routes>
